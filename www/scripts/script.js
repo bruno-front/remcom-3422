bb8f04917f7cadd6280d4f7e67e51474b4f6b6ad
Kirill Demidov <kademidov@gmail.com>
2024-07-03 00:26:53 +0300

    #14 Занятие. js, jQuery. Сделано выпадающее меню на мобильной и планшетной версиях.

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,56 @@
-console.log('script work!');
+$(document).ready(function() {
+
+  let isOpen = false;
+
+  $('.js-burger').on('click', function() {
+
+    if (isOpen) {
+      $('.js-main-nav').slideUp();
+      isOpen = false;
+    } else {
+      $('.js-main-nav').slideDown();
+      isOpen = true;
+    }
+
+  });
+
+  // Простая альтернатива:
+  // $('.js-burger').on('click', function() {
+
+  //   $('.js-main-nav').slideToggle();
+
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
