791db3f6c730fe63a35c8c110ce9b27a77ddbca4
Kirill Demidov <kademidov@gmail.com>
2024-07-05 19:30:48 +0300

    #16 Занятие. js, jQuery. аккордеоны, табы

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -22,15 +22,39 @@ $(document).ready(function() {
   // });
 
 
+  let prevAccordionBtn;
+
+  $('.js-accordion-btn').on('click', function() {
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.js-accordion-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevAccordionBtn = this;
+
+  });
 
 
 
 
+  const tabActiveClass = 'active';
+  const $tabsLink = $('.js-tabs-link');
+  const $tabsContent = $('.js-contact-card');
 
+  $tabsLink.on('click', function(event) {
+    event.preventDefault();
 
+    const index = $(this).index('.js-tabs-link');
+    console.log(index);
 
+    $tabsLink.removeClass(tabActiveClass);
+    $(this).addClass(tabActiveClass);
 
-
+    $tabsContent.removeClass(tabActiveClass);
+    $tabsContent.eq(index).addClass(tabActiveClass);
+  });
 
 
 
